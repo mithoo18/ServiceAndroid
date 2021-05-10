d096960aca05b269398672467f3358ed9ff6c98b
mits <devanshu18g@gmail.com>
2021-05-10 17:01:27 +0530

    Completed & Tested

--- app/src/main/java/com/example/serviceandroid/MainActivity.java
+++ app/src/main/java/com/example/serviceandroid/MainActivity.java
@@ -1,8 +1,15 @@
 package com.example.serviceandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.widget.Toolbar;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Check", Toast.LENGTH_SHORT).show();
+                startService(new Intent(MainActivity.this,MyService.class));
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/serviceandroid/MyService.java
+++ app/src/main/java/com/example/serviceandroid/MyService.java
@@ -4,11 +4,12 @@ import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
 import android.view.View;
-
+import android.widget.Toast;
 import androidx.annotation.Nullable;
-
 public class MyService extends Service {
 
+    public MyService() {
+    }
 
     @Nullable
     @Override
@@ -19,15 +20,22 @@ public class MyService extends Service {
     @Override
     public void onCreate() {
         super.onCreate();
+        Toast.makeText(this, "OnCreate", Toast.LENGTH_SHORT).show();
+        System.out.println("onCreate");
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+        System.out.println("onStartCommand");
+        Toast.makeText(this, "Service Started", Toast.LENGTH_SHORT).show();
         return super.onStartCommand(intent, flags, startId);
+
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
+        System.out.println("OnDestroy");
+        Toast.makeText(this, "OnDestroy", Toast.LENGTH_SHORT).show();
     }
 }
